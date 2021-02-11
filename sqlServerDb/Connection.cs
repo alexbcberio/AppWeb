838fc41c0aebc7ee3b44d12369d993241b8b0ecb
alexbcberio <alexbcberio@gmail.com>
2021-02-11 22:25:48 +0100

    ✨ Added sqlServerDb solution
    Make SQL queries to SqlServer

---
+++ sqlServerDb/Connection.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace sqlServerDb
+{
+    public class Connection
+    {
+        private bool connected;
+        private SqlConnection connection;
+
+        public Connection()
+        {
+            string connectionString = "CONNECTION_STRING";
+            connected = false;
+            connection = new SqlConnection(connectionString);
+        }
+
+        /*
+         * Opens a connection to the database
+         */
+        public void Open()
+        {
+            if (!connected)
+            {
+                try
+                {
+                    connection.Open();
+                    connected = true;
+                } catch (Exception e)
+                {
+                    Console.WriteLine("Error opening connection to database.");
+                    Console.Write(e.Message);
+                }
+            }
+        }
+
+        /*
+         * Closes the connection with the database
+         */
+        public void Close()
+        {
+            if (connected)
+            {
+                connection.Close();
+                connected = false;
+            }
+        }
+
+        /*
+         * Executes the given query returning a scalar.
+         */
+        public object ExecuteScalar(string query)
+        {
+            SqlCommand command = createCommand(query);
+
+            return command.ExecuteScalar();
+        }
+
+        /*
+         * Executes the given query returning the number of affected rows.
+         */
+        public int ExecuteNonQuery(string query)
+        {
+            SqlCommand command = createCommand(query);
+
+            return command.ExecuteNonQuery();
+        }
+
+        /*
+         * Executes the given query returning a SqlDataReader.
+         */
+        public SqlDataReader ExecuteReader(string query)
+        {
+            SqlCommand command = createCommand(query);
+
+            return command.ExecuteReader();
+        }
+
+        /*
+         * Opens a connection if its not already connected and creates a SqlCommand.
+         */
+        private SqlCommand createCommand(string query)
+        {
+            if (!connected)
+            {
+                Open();
+            }
+
+            return new SqlCommand(query, connection);
+        }
+    }
+}
